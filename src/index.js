0b22a41768e88bae8c5feef6d4282ab80f8f7d16
Tomáš Mrázek <tomasmrazek92@gmail.com>
2025-09-04 22:47:42 +0200

    Arrows Unique Class Update

--- src/index.js
+++ src/index.js
@@ -1,49 +1,47 @@
 import { runSplit } from './utils/globalFunctions';
 
 function initDesktopSwiper() {
-  let swiper;
+  $('.ai-slider').each(function (index) {
+    let swiper;
+    let $currentSlider = $(this);
+    let $section = $currentSlider.closest('section');
+    let uniqueId = 'slider-' + index;
 
-  // Function to check window width and initialize or destroy Swiper accordingly
-  function checkWidth() {
-    const windowWidth = $(window).width();
+    let $nextArrow = $section.find('.swiper-arrow.next');
+    let $prevArrow = $section.find('.swiper-arrow.prev');
 
-    let sliderWrap = $('.ai-slider');
+    $nextArrow.addClass(uniqueId + '-next');
+    $prevArrow.addClass(uniqueId + '-prev');
 
-    if (!sliderWrap.length) return;
+    function checkWidth() {
+      const windowWidth = $(window).width();
 
-    // If window width is 992px or more and swiper doesn't exist
-    if (windowWidth >= 992 && (!swiper || swiper.destroyed)) {
-      // Initialize Swiper
-      swiper = new Swiper('.ai-slider', {
-        // Your Swiper options here
-        slidesPerView: 'auto',
-        spaceBetween: 32,
-        navigation: {
-          nextEl: '.swiper-arrow.next',
-          prevEl: '.swiper-arrow.prev',
-        },
-      });
+      if (windowWidth >= 992 && (!swiper || swiper.destroyed)) {
+        swiper = new Swiper($currentSlider[0], {
+          slidesPerView: 'auto',
+          spaceBetween: 32,
+          navigation: {
+            nextEl: '.' + uniqueId + '-next',
+            prevEl: '.' + uniqueId + '-prev',
+          },
+        });
+      } else if (windowWidth < 992 && swiper && !swiper.destroyed) {
+        swiper.destroy(true, true);
+        swiper = undefined;
+      }
     }
-    // If window width is less than 992px and swiper exists
-    else if (windowWidth < 992 && swiper && !swiper.destroyed) {
-      // Destroy Swiper instance
-      swiper.destroy(true, true);
-      swiper = undefined;
-    }
-  }
 
-  // Check width on page load
-  $(document).ready(function () {
-    checkWidth();
-  });
-
-  // Check width on window resize (with debounce for better performance)
-  let resizeTimer;
-  $(window).resize(function () {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function () {
+    $(document).ready(function () {
       checkWidth();
-    }, 250);
+    });
+
+    let resizeTimer;
+    $(window).resize(function () {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(function () {
+        checkWidth();
+      }, 250);
+    });
   });
 }
 function resultsList() {
